1dcac1af3552eba9cfe896427cf5cad1e72dc1f9
George <gmperry18@gmail.com>
2019-02-06 14:21:44 +0000

    Testing config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            George Perry
+            George Perry's Website
           </p>
           <a
             className="App-link"
